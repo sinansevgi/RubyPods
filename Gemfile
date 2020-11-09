d0dd98a406013c59fa3269da44c87e4d8d2a83e4
Sinan Sevgi <sinansevgi@users.noreply.github.com>
2020-11-09 22:39:40 +0300

    Feature add.
    main.rb:
    -User actions created for main.rb
    -Progress bar added for fetch.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'nokogiri'
 gem 'open-uri'
+gem 'progress_bar'
--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,7 @@
 require_relative '../libs/podscraper'
 require_relative '../libs/podlist'
 require_relative '../libs/pageupdater'
+require 'progress_bar'
 
 system('clear')
 puts '██████╗ ██╗   ██╗██████╗ ██╗   ██╗    ██████╗  ██████╗ ██████╗  ██████╗ █████╗ ███████╗████████╗███████╗'
@@ -11,33 +12,60 @@ puts '██║  ██║╚██████╔╝██████╔╝   
 puts '╚═╝  ╚═╝ ╚═════╝ ╚═════╝    ╚═╝       ╚═╝      ╚═════╝ ╚═════╝  ╚═════╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝'
 puts "\n\nPress Enter to for menu"
 podcast_list = Pods.new
-podcast_list.pod_links.each do |podcast|
-  podder = Feed.new(podcast)
-  page_create = Pager.new(podder.title, podder.pod_image, podder.result_titles, podder.result_links)
-  page_create.page_update
+
+begin
+  system('clear')
+  puts 'Welcome to Ruby Podcasts'
+  puts '1 - Podcast List Options'
+  puts '2 - Fetch Latest Podcasts'
+  puts 'q - Quit'
+  input_opt = gets.chomp
+  raise unless input_opt.include?('1') || input_opt.include?('2') || input_opt.include?('q')
+  case input_opt
+  when '1'
+    system('clear')
+    loop do
+      puts 's - See available podcats'
+      puts 'a - Add new podcast'
+      puts 'd - Delete podcast'
+      puts 'q - Quit'
+      input_opt = gets.chomp
+      case input_opt
+      when 's'
+        podcast_list.pod_links.each{|podcast| puts podcast}
+        next
+      when 'a'
+        puts "Please enter the rss url that you would like to add to podcast list :"
+        pod_url = gets.chomp
+        podcast_list.add_pod(pod_url)
+        next
+      when 'd'
+        podcast_list.pod_links.each{|podcast| puts podcast}
+        puts "Please enter the rss url that you would like to delete from podcast list :"
+        pod_url = gets.chomp
+        podcast_list.remove_pod(url)
+        next
+      else
+        break
+      end
+      break if input_opt == 'q'
+    end
+  when '2'
+    bar = ProgressBar.new
+    system('clear')
+    puts "Please wait while your results getting created."
+    podcast_list.pod_links.each do |podcast|
+      bar.increment!
+      podder = Feed.new(podcast)
+      page_create = Pager.new(podder.title, podder.pod_image, podder.result_titles, podder.result_links)
+      page_create.page_update
+    end
+    puts "Your results created in result.html file."
+    puts "Please open results.html with your browser."
+    end
+
+rescue StandardError
+  retry
 end
-# begin
-#   system('clear')
-#   puts 'Welcome to Ruby Podcasts'
-#   puts '1 - Podcast List Options'
-#   puts '2 - Fetch Latest Podcasts'
-#   puts 'q - Quit'
-#   input_opt = gets.chomp
-#   raise unless input_opt.include?('1') || input_opt.include?('2') || input_opt.include?('q')
-#   case input_opt
-#   when '1'
-#     system('clear')
-#     loop do
-#       puts 's - See available podcats'
-#       puts 'a - Add new podcast'
-#       puts 'd - Delete podcast'
-#       puts 'q - Quit'
-#       break if input_opt == 'q'
-#     end
-#   when '2'
-#
-#   end
-#
-# rescue StandardError
-#   retry
-# end
+
+
